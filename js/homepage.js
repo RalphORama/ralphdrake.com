9eba316a98850f928227cdc5f2840ea1b2cf9ec1
Ralph Drake <rama.o.ralph@gmail.com>
2017-02-10 18:10:21 -0500

    Added an example JSON file for #1, removed all DS_Store files

--- js/homepage.js
+++ js/homepage.js
@@ -2,7 +2,8 @@
   var today = new Date()
   var footer = document.getElementsByTagName('footer')[0]
 
-  footer.innerHTML = '&copy; ' + today.getFullYear() + ' Ralph Drake'
+  footer.innerHTML = '<span>&copy; ' + today.getFullYear()
+    + ' Ralph Drake</span>'
 
   $('a[href="#info"]').click(function() {
     $('html, body').animate({scrollTop: $('#info').offset().top}, 1 * 1000)
