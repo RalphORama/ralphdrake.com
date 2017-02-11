e0cd80aace4097fae7d657fe65ded8b3817d48a5
Ralph Drake <rama.o.ralph@gmail.com>
2017-02-11 15:58:45 -0500

    Created simple navagation

--- js/homepage.js
+++ js/homepage.js
@@ -1,12 +1,20 @@
-(function() {
+/* global $ */
+(function () {
   var today = new Date()
   var footer = document.getElementsByTagName('footer')[0]
 
-  footer.innerHTML = '<span>&copy; ' + today.getFullYear()
-    + ' Ralph Drake</span>'
+  footer.innerHTML = '<span>&copy; ' + today.getFullYear() + ' Ralph Drake</span>'
 
-  $('a[href="#info"]').click(function() {
-    $('html, body').animate({scrollTop: $('#info').offset().top}, 1 * 1000)
-    return false;
-  })
+  addScrollListener('a[href="#info"]', '#info')
+  addScrollListener('a[href="#intro"]', '#intro')
+  addScrollListener('a[href="#projects"]', '#projects')
+
+  function addScrollListener (elem, target, delay = 1) {
+    $(elem).click(function () {
+      $('html, body').animate({scrollTop: $(target).offset().top}, delay * 1000)
+      return false
+    })
+    console.log('Created listener for ' + elem + ' to ' + target + ' with delay of ' +
+        delay + '.')
+  }
 })()
