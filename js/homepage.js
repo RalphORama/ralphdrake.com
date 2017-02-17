d30b2770d197f7f783d883ea003c1baa7b21b63f
Ralph Drake <rama.o.ralph@gmail.com>
2017-02-17 18:05:50 -0500

    Kind of got the JT working. Not really.

--- js/homepage.js
+++ js/homepage.js
@@ -3,16 +3,41 @@
   var today = new Date()
   var footer = document.getElementsByTagName('footer')[0]
 
-  footer.innerHTML = '<span>&copy; ' + today.getFullYear() + ' Ralph Drake</span>'
-
-  addScrollListener('a[href="#info"]', '#info')
-  addScrollListener('a[href="#intro"]', '#intro')
-  addScrollListener('a[href="#projects"]', '#projects')
-
   function addScrollListener (elem, target, delay = 1) {
     $(elem).click(function () {
       $('html, body').animate({scrollTop: $(target).offset().top}, delay * 1000)
       return false
     })
   }
+
+  function addJumbotronPanel (num, title, description, notes) {
+    var jt = document.getElementById('jumbotron')
+    var offset = num * window.screen.width
+    var notesText = ''
+
+    for (var i = 0; i < notes.length; ++i) {
+      notesText += `<li>${notes[i]}</li>`
+    }
+
+    var newPanel = `
+    <div class="jt-panel" style="margin-left: ${offset}px;">
+      <h1>${title}</h1>
+      <p>${description}</p>
+      <ul>${notesText}</ul>
+    </div>`
+
+    jt.innerHTML += newPanel
+  }
+
+  addScrollListener('a[href="#info"]', '#info')
+  addScrollListener('a[href="#intro"]', '#intro')
+  addScrollListener('a[href="#projects"]', '#projects')
+  footer.innerHTML = '<span>&copy; ' + today.getFullYear() + ' Ralph Drake</span>'
+
+  $.getJSON('/projects.json', function (jdata) {
+    var count = 0
+    $.each(jdata, function (_, key) {
+      addJumbotronPanel(count++, key['name'], key['accomplishments'], key['notes'])
+    })
+  })
 })()
