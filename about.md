779e497e0126d2306a2f3239e7b0b74f088f8ca3
Ralph Drake <rama.o.ralph@gmail.com>
2017-02-22 15:12:02 -0500

    Added showdown.js to parse a markdown file for creating the 'about' section

---
+++ about.md
@@ -0,0 +1,7 @@
+![headshot](/img/headshot.jpg)
+
+Hi. I'm Ralph Drake. I code, I act, I play bass guitar. Most importantly, I'm an innovator. Over the years, I've taught myself many things - how to make websites, games, even how to manage my own Linux server in its entirety. I like to tinker. A lot.
+
+There's not much more for me to say about myself. I have a resume here if you're interested in hiring me. I'm a college student - wide-eyed and naieve. There's probably some more stuff I should write about myself here, but I'm not sure what to put. I'll fix that later.
+
+Oh wow yeah I have some accomplishments, most notably Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel sodales magna. Ut nec risus dignissim, molestie ligula laoreet, commodo massa. Aenean in placerat erat, sed gravida velit. Vestibulum rutrum vehicula ante, id egestas odio euismod a. Ut porta, nulla nec tincidunt tincidunt, ex mi interdum nisl, sit amet malesuada magna velit in elit. Duis vel ornare orci, ut maximus felis. Vestibulum ornare eros a euismod sollicitudin.
--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,18 @@
-/* global $ */
+/* global $ showdown */
 (function () {
   var today = new Date()
+
+  var converterOptions = {
+    'noHeaderId': false,
+    'simplifiedAutoLink': true,
+    'tasklists': true
+  }
+  var converter = new showdown.Converter(converterOptions)
+
   var footer = document.getElementsByTagName('footer')[0]
+
+  var aboutFile = '/about.md'
+  var projectsFile = '/projects.json'
 
   function addNavListener (elem, target, delay = 1) {
     $(elem).click(function () {
@@ -20,7 +31,7 @@
 
     var newPanel = `
     <div class="project">
-      <a href="https://${title}"><h1>${title}</h1></a>
+      <h1><a href="https://${title}">${title}</a></h1>
       <p>${description}</p>
       <ul>${notesText}</ul>
       <hr>
@@ -29,14 +40,19 @@
     projectsSeciton.innerHTML += newPanel
   }
 
-  // Cre
   addNavListener('a[href="#info"]', '#info')
   addNavListener('a[href="#intro"]', '#intro')
   addNavListener('a[href="#projects"]', '#projects')
 
   footer.innerHTML = '<span>&copy; ' + today.getFullYear() + ' Ralph Drake</span>'
 
-  $.getJSON('/projects.json', function (jdata) {
+  // Populate the about section with our markdown file
+  $.get(aboutFile, function (data) {
+    $('.description').html(converter.makeHtml(data))
+  })
+
+  // Automatically populate the 'projects' section
+  $.getJSON(projectsFile, function (jdata) {
     $.each(jdata, function (key, value) {
       addProject(key, value['accomplishments'], value['notes'])
     })
